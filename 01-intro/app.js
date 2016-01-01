28555947bb7b709078e1fadeb00b296545447836
Joe Maddalone <joe@insert-title.com>
2016-01-01 16:29:34 -0600

    intro lesson

---
+++ 01-intro/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const output = (str, regex, target) => {
+  target.innerHTML =
+    str.replace(regex, str => `<span>${str}</span>`);
+}
+
+var str = `Is this This?`;
+
+//var regex = new RegExp("is", "g");
+var regex = /is/gi;
+
+output(str, regex, document.querySelector('pre'))
+
+// console.log(regex.test(str));
+// console.log(regex.exec(str));
+// console.log(regex.exec(str));
+// console.log(regex.exec(str));
+// console.log(regex.exec(str));
+
+//console.log(str.match(regex));
+
+//console.log(str.replace(regex, str => "XX"));
+
+//console.log(str.search(regex))
+
+
